2684d5c2d950bfa3c771164f46ff00465d0fd1bb
ykmxxi <ykmxxi97@gmail.com>
2024-08-27 17:39:10 +0900

    feat: 모든 요청 인증없이 접근 가능

--- src/main/java/com/example/crud/config/SecurityConfig.java
+++ src/main/java/com/example/crud/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.crud.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
@@ -16,7 +18,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
-        return http.httpBasic(Customizer.withDefaults())
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
+                .authorizeHttpRequests(customizer -> customizer.anyRequest().permitAll())
                 .build();
     }
 
